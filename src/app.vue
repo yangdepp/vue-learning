7e0fbe8bada049cb50480f63177a02961b952520
yangdepp <965093975@qq.com>
2018-08-27 14:24:18 +0800

    header footer样式

--- src/app.vue
+++ src/app.vue
@@ -1,17 +1,45 @@
 <template>
-  <div>{{text}}</div>
+  <div id="app">
+    <div id="cover">
+       <Header></Header>
+       <Todo></Todo>
+       <Footer></Footer>
+    </div>
+  </div>
 </template>
 <script>
+import Header from './todo/header.vue';
+import Footer from './todo/footer.jsx';
+import Todo from './todo/todo.vue';
 export default {
   data(){
     return{
       text: 'abc'
     }
+  },
+  components:{
+    Header,
+    Footer,
+    Todo
   }
 }
 </script>
-<style>
-  .text{
-    color: red;
-  }
+<style lang="stylus">
+    #app {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+    }
+    #cover {
+        position: absolute;
+        left: 0;
+        right: 0;
+        top: 0;
+        bottom: 0;
+        background-color: #999;
+        opacity: .8;
+        z-index: -1;
+    }
 </style>
---
+++ src/todo/header.vue
@@ -0,0 +1,21 @@
+<template>
+  <header class="main-header">
+    <h1>JTodo</h1>
+  </header>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="stylus" scoped>
+  .main-header {
+      text-align center
+      h1 {
+          font-size 100px
+          color rgba(175, 47, 47, .4)
+          font-weight 400
+          margin 20px
+      }
+  }
+</style>
