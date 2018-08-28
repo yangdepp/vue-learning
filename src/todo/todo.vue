ab870cb54d54c02c7f1d6c81703efcf9bd7b33b0
yangdepp <965093975@qq.com>
2018-08-28 09:57:04 +0800

    fix输入空格bug

--- src/todo/todo.vue
+++ src/todo/todo.vue
@@ -30,7 +30,7 @@ export default {
   },
   methods: {
     addTodo() {
-      if(this.inputValue){
+      if(this.inputValue.trim()){
         this.todos.unshift({
           id: id++,
           content: this.inputValue,
